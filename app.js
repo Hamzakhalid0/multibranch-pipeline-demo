13a53b0b243e57d2878d3d605dba1118e7ae7669
Hamza Khalid <info.hamzalizer@gmail.com>
2022-04-05 10:32:41 +0500

    s

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('This application is running on slave node. Modified!')
+  res.send('This application is running on slave node. New changes are made!')
 })
 
 app.listen(3000, function () {
