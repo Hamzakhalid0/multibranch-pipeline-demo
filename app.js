737ad31d305032b48d9336b3f28228d56690cf15
Hamza Khalid <info.hamzalizer@gmail.com>
2022-04-06 15:23:36 +0500

    s

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('This page running on master/dev node.')
+  res.send('This page running on master/dev node123.')
 })
 
 app.listen(3000, function () {
